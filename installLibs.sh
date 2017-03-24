13312e178d534ee1a6522e46ea44507e4a2575a2
Nikita, linux <bondartsev.nikita@gmail.com>
2017-03-24 13:00:59 +0300

    Changed installLibs.sh to contain the right newline symbols

--- installLibs.sh
+++ installLibs.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#скачиваем и распаковываем LAPACK
+#скачиваем и распаковывем LAPACK
 mkdir ../buildLapack
 cd ../buildLapack
 wget http://www.netlib.org/lapack/lapack-3.7.0.tgz
 tar zxvf lapack-3.7.0.tgz
 cd lapack-3.7.0
-#Используем makefile по умолчанию, но можно его менять, если нужно что-то особенное
+#Используем makefile по умолчанию
 mv make.inc.example make.inc
-#Собираем BLAS. После этого в папке lapack-3.7.0 должен появиться файл librefblas.a
+#Собираем BLAS. Появится librefblas.a
 make blaslib
-#Собираем LAPACK. Результат liblapack.a
+#Собираем LAPCK. Появится liblapack.a
 make lapacklib
 mv librefblas.a ../../lspin/libblas.a
 mv liblapack.a ../../lspin/liblapack.a
